b8877fe78dcf946a16460a0b0652c55ab44dcbee
neftaliweblab <neftali.weblab@gmail.com>
2026-05-14 18:18:19 -0600

    Agregar Nuevas Bases al Worker

--- config.js
+++ config.js
@@ -26,11 +26,16 @@ const VALID_EVENT_TYPES = [
  */
 const buildDbMap = () => {
   const candidates = [
-    { dsId: process.env.DB_PROGRAMAS_Y_PROYECTOS, config: { tipo: 'Proyecto', origen: 'PROGRAMAS Y PROYECTOS', relacion: 'Proyecto' } },
-    { dsId: process.env.DB_REVISIONES,            config: { tipo: 'Revisión', origen: 'REVISIONES',            relacion: 'Revisión' } },
-    { dsId: process.env.DB_USUARIOS,              config: { tipo: 'Cliente',  origen: 'USUARIOS',              relacion: 'Usuario' } },
-    { dsId: process.env.DB_COBRAR_Y_PAGAR,        config: { tipo: 'Factura',  origen: 'COBRAR Y PAGAR',        relacion: 'Cobranza' } },
-    { dsId: process.env.DB_SERVIDORES,            config: { tipo: 'Dominio',  origen: 'SERVIDORES',            relacion: 'Servidor' } }
+    { dsId: process.env.DB_PROGRAMAS_Y_PROYECTOS, config: { tipo: 'Proyecto', origen: 'PROGRAMAS Y PROYECTOS', relacion: 'Programas y Proyectos' } },
+    { dsId: process.env.DB_REVISIONES,            config: { tipo: 'Revisión', origen: 'REVISIONES',            relacion: 'Revisiones' } },
+    { dsId: process.env.DB_USUARIOS,              config: { tipo: 'Cliente',  origen: 'USUARIOS',              relacion: 'Clientes' } },
+    { dsId: process.env.DB_COBRAR_Y_PAGAR,        config: { tipo: 'Factura',  origen: 'COBRAR Y PAGAR',        relacion: 'COBRAR Y PAGAR' } },
+    { dsId: process.env.DB_SERVIDORES,            config: { tipo: 'Dominio',  origen: 'SERVIDORES',            relacion: 'Dominios y WP (Contraseñas) Servers Accesos' } },
+    { dsId: process.env.DB_REV2,                  config: { tipo: 'Revisión', origen: 'REV2',                  relacion: 'REV2' } },
+    { dsId: process.env.DB_DOMINIO_WP_PANEL,      config: { tipo: 'Dominio',  origen: 'DOMINIO WP PANEL',      relacion: 'Dominio Wordpress Panel y Correos Contraseñas y siteground,REDES' } },
+    { dsId: process.env.DB_DOMINIO_REGISTRANTE,   config: { tipo: 'Dominio',  origen: 'DOMINIO REGISTRANTE',   relacion: 'Dominio registrante GoDaddy Akky Nubox'} },
+    { dsId: process.env.DB_INSPIRACION,           config: { tipo: 'Inspiración', origen: 'INSPIRACION',        relacion: 'INSPIRACION' } },
+    { dsId: process.env.DB_PROVEEDORES,           config: { tipo: 'Proveedor', origen: 'PROVEEDORES',          relacion: 'PROVEEDORES' } }
   ];
 
   const map = {};
